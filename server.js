4b7368146153dd08af99b6d79fb5888bd0c0c81b
pratyushlohumi <pratyushlohumi26@gmail.com>
2016-09-21 20:59:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,73 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'Article One\ Pratyush Lohumi',
+    heading: 'Article One',
+    date: 'september 20th 016',
+    content: `
+            <p>
+                With the league stage coming to an end, we have seen not only some excellent plays and new talents coming into the spotlight, we also saw an incredible 11 match win streak end.All the teams played extremely well, but only the best have reached the playoffs. Hard luck for those not being able to qualify and wishing them the best for the next season.
+            </p>
+            
+             <p>
+                With the league stage coming to an end, we have seen not only some excellent plays and new talents coming into the spotlight, we also saw an incredible 11 match win streak end.All the teams played extremely well, but only the best have reached the playoffs. Hard luck for those not being able to qualify and wishing them the best for the next season.
+            </p>
+            
+             <p>
+                With the league stage coming to an end, we have seen not only some excellent plays and new talents coming into the spotlight, we also saw an incredible 11 match win streak end.All the teams played extremely well, but only the best have reached the playoffs. Hard luck for those not being able to qualify and wishing them the best for the next season.
+            </p>`
+};
+function createTemplate(data){
+var title=data.title;
+var date=data.date;
+var heading= data.heading;
+var content=data.content;
+
+var htmlTemplate= `
+<html>
+    <head>
+        <title>
+            Article One | Pratyush Lohumi
+        </title>
+        <meta name="viewport" content="width=device-width, intial-scale=1"/>
+          <link href="/ui/style.css" rel="stylesheet" />
+    
+    
+    </head>
+    
+    <body>
+        <div class="container">
+        <div>
+        <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>    
+            <div>
+                ${date}
+            </div>
+        <div>
+            
+                ${content}
+            
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return html.Template;
+    
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOn));
 });
 
 app.get('/article-two',function(req,res){
