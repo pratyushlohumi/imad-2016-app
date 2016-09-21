9fcbc3fb3aa9cbc0fdefdbdbbdbc20123f0bd35e
pratyushlohumi <pratyushlohumi26@gmail.com>
2016-09-21 21:31:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,42 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleOne={
+var articles = {
+    'article-one':{
+    title:'Article One\ Pratyush Lohumi',
+    heading: 'Article One',
+    date: 'september 20th 016',
+    content: `
+            <p>
+                With the league stage coming to an end, we have seen not only some excellent plays and new talents coming into the spotlight, we also saw an incredible 11 match win streak end.All the teams played extremely well, but only the best have reached the playoffs. Hard luck for those not being able to qualify and wishing them the best for the next season.
+            </p>
+            
+             <p>
+                With the league stage coming to an end, we have seen not only some excellent plays and new talents coming into the spotlight, we also saw an incredible 11 match win streak end.All the teams played extremely well, but only the best have reached the playoffs. Hard luck for those not being able to qualify and wishing them the best for the next season.
+            </p>
+            
+             <p>
+                With the league stage coming to an end, we have seen not only some excellent plays and new talents coming into the spotlight, we also saw an incredible 11 match win streak end.All the teams played extremely well, but only the best have reached the playoffs. Hard luck for those not being able to qualify and wishing them the best for the next season.
+            </p>`
+},
+    'article-two':{
+    title:'Article One\ Pratyush Lohumi',
+    heading: 'Article One',
+    date: 'september 20th 016',
+    content: `
+            <p>
+                With the league stage coming to an end, we have seen not only some excellent plays and new talents coming into the spotlight, we also saw an incredible 11 match win streak end.All the teams played extremely well, but only the best have reached the playoffs. Hard luck for those not being able to qualify and wishing them the best for the next season.
+            </p>
+            
+             <p>
+                With the league stage coming to an end, we have seen not only some excellent plays and new talents coming into the spotlight, we also saw an incredible 11 match win streak end.All the teams played extremely well, but only the best have reached the playoffs. Hard luck for those not being able to qualify and wishing them the best for the next season.
+            </p>
+            
+             <p>
+                With the league stage coming to an end, we have seen not only some excellent plays and new talents coming into the spotlight, we also saw an incredible 11 match win streak end.All the teams played extremely well, but only the best have reached the playoffs. Hard luck for those not being able to qualify and wishing them the best for the next season.
+            </p>`
+}, 
+    'article-three':{
     title:'Article One\ Pratyush Lohumi',
     heading: 'Article One',
     date: 'september 20th 016',
@@ -70,8 +104,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-    res.send(createTemplate(articleOn));
+app.get('/:articleName',function(req,res){
+    var articleName= req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two',function(req,res){
